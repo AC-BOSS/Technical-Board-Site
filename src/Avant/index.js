e3a10533cc80200fb0f755d8d5fad6770749a95b
Ankit Kumar <53820439+iamankitkumar08062001@users.noreply.github.com>
2021-02-18 15:39:44 +0530

    Added Skeleton Loaders

--- src/Avant/index.js
+++ src/Avant/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { SkeletonTheme } from "react-loading-skeleton";
 import Pages from './pages';
 import './avant.module.css';
 
 function Avant(props) {
     return (
-        <Switch>
-            <Route path="/avant/gallery" component={Pages.Gallery} />
-            <Route path="/avant/events" component={Pages.Event} />
-            <Route path="/avant/past-coordinators" component={Pages.PastCoordinators} />
-            <Route path="/avant/resources" component={Pages.Resources} />
-            <Route exact path="/avant" component={Pages.Home} />
-        </Switch>
+        <SkeletonTheme color="#202020" highlightColor="#444">
+            <Switch>
+                <Route path="/avant/gallery" component={Pages.Gallery} />
+                <Route path="/avant/events" component={Pages.Event} />
+                <Route path="/avant/past-coordinators" component={Pages.PastCoordinators} />
+                <Route path="/avant/resources" component={Pages.Resources} />
+                <Route exact path="/avant" component={Pages.Home} />
+            </Switch>
+        </SkeletonTheme>
     );
 }
 
